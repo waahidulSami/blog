7d9d2f48029e6cde600f66e4758b33be8bd72adc
wahidul islam sami <wahidulislamsami5@gmail.com>
2025-07-04 22:27:29 +0600

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
-import './input.css'
+import './index.css'
 import App from './App.jsx'
 import store from './store/store.js'
 import AuthLayout from './components/AuthLayout.jsx'
